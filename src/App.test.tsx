69340750687812452ea7004981550bb63eb3c91a
Carlos Arellano <ing_carlosarellano@hotmail.com>
2024-08-15 19:30:24 -0700

    - Updated husky pre-commit. - Added condition to force a 100% code coverage. - Updated test file of App component.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-	render(<App />);
+	act(() => {
+		render(<App />);
+	});
 	const linkElement = screen.getByText(
 		/Hello world, welcome to Lighthouse Shop Template!/i,
 	);
