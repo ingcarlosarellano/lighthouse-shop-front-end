86956f51506a5f358bd5759ab9c544df47c93fb9
Carlos Arellano <ing_carlosarellano@hotmail.com>
2024-08-15 17:24:11 -0700

    - Added husky pre-commit commands. - Updated App.tsx in order to test pre-commit functionality. - Deleted unnecesary test file of index.tsx.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,8 @@ import App from './App';
 
 test('renders learn react link', () => {
 	render(<App />);
-	const linkElement = screen.getByText(/Hello world!/i);
+	const linkElement = screen.getByText(
+		/Hello world, welcome to Lighthouse Shop Template!/i,
+	);
 	expect(linkElement).toBeInTheDocument();
 });
